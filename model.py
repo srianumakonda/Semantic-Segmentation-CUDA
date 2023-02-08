28b1667ca226f71262cfede7cc08621001fa107d
srianumakonda <sri.anumakonda06@gmail.com>
2023-02-07 21:26:28 -0600

    addded U-Net model along with helper functions

---
+++ model.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.utils import *
+
+class CityScapesNetwork(nn.Module):
+    
+    def __init__(self, in_channels, out_channels):
+        
+        """
+        @param:
+            in_channels (int): specify the number of input channels for the image
+            out_channels (int): specify the number of output channels to be released from the U-Net model
+        """
+
+        super(UNet, self).__init__()
+        
+        self.down1 = double_conv(in_channels,64)
+        self.down2 = max_down(64,128)
+        self.down3 = max_down(128,256)
+        self.down4 = max_down(256,512)
+        self.down5 = max_down(512,512)
+        self.up1 = upsample(1024,256)
+        self.up2 = upsample(512,128)
+        self.up3 = upsample(256,64)
+        self.up4 = upsample(128,64)
+        self.conv1 = nn.Conv2d(64, out_channels, 1)
+	self.out_conv = nn.Conv2d(out_channels, out_channels, 1)	
+
+    def forward(self, x):
+
+        x1 = self.down1(x)
+        x2 = self.down2(x1)
+        x3 = self.down3(x2)
+        x4 = self.down4(x3)
+        x5 = self.down5(x4)
+        x = self.up1(x5,x4)
+        x = self.up2(x,x3)
+        x = self.up3(x,x2)
+        x = self.up4(x,x1)
+	x = self.conv1(x)
+        out = self.out_conv(x)
+        return out
---
+++ utils/utils.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Residual_Block(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, stride):
+        super(Residual_Block, self).__init__()
+        
+        self.relu = nn.ReLU()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1, stride=stride)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1, stride=1)
+        self.skip_conv = nn.Conv2d(in_channels, out_channels, 1, padding=0, stride=stride)
+        
+        self.in_1 = nn.InstanceNorm2d(in_channels, affine=True)
+        self.in_2 = nn.InstanceNorm2d(out_channels, affine=True)
+
+    def forward(self, x):
+        
+        res_block = self.relu(self.in_1(x))
+        res_block = self.conv1(res_block)
+        res_block = self.relu(self.in_2(res_block))
+        res_block = self.conv2(res_block)
+        s = self.skip_conv(x)
+        skip = res_block + s
+        return skip
+
+class Decoder_Block(nn.Module):
+    
+    def __init__(self, in_channels, out_channels):
+        super(Decoder_Block, self).__init__()
+
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.res_block = Residual_Block(in_channels + out_channels, out_channels, stride=1)
+
+    def forward(self, x, skip):
+        
+        dec_block = self.upsample(x)
+        dec_block = torch.cat([dec_block, skip], axis=1)
+        dec_block = self.res_block(dec_block)
+        return dec_block
+
+class double_conv(nn.Module):
+
+    def __init__(self, in_channels,out_channels,kernel_size=3,stride=1,padding=1,maxpool=False):
+        super(double_conv,self).__init__()
+
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels,out_channels,kernel_size=kernel_size,stride=stride,padding=padding),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels,out_channels,kernel_size=kernel_size,stride=stride,padding=padding),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU()   
+        )
+
+    def forward(self, x):
+        x = self.double_conv(x)
+        return x
+
+class max_down(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super(max_down,self).__init__()
+
+        self.max_down = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_channels,out_channels)
+        )
+
+    def forward(self, x):
+        x = self.max_down(x)
+        return x
+
+class upsample(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, in_conv=None, bilinear=False):
+        super(upsample,self).__init__()
+
+        if bilinear:
+            self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+        else:
+            self.upsample = nn.ConvTranspose2d(in_channels//2, in_channels//2, kernel_size=2, stride=2)
+        self.conv = double_conv(in_channels, out_channels)
+        
+        if in_conv != None:
+            self.conv = double_conv(in_conv, out_channels)
+            if bilinear:
+                self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+            else:
+                self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+    
+    def forward(self, x1, x2):
+        x1 = self.upsample(x1)
+        diffX = x1.size()[2] - x2.size()[2]
+        diffY = x1.size()[3] - x2.size()[3]
+        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
+                        diffY // 2, int(diffY / 2)))
+        
+        out = torch.cat([x2, x1], dim=1)
+        out = self.conv(out)
+        return out
